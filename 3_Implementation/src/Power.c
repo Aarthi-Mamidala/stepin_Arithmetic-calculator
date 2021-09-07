f604cde67f6e08d9901bbd558199c5e3dcb0e9ef
Aarthi-Mamidala <89648059+Aarthi-Mamidala@users.noreply.github.com>
2021-09-07 19:09:11 +0530

    Update Power.c

--- 3_Implementation/src/Power.c
+++ 3_Implementation/src/Power.c
@@ -1,1 +1,14 @@
-
+#include"Function.h"
+  /**
+ * @brief  finding  the Power of the number
+ * 
+ * @param A operand1
+ * @param B operand2
+ * @return int  it returns the output value
+ */
+int Power(int A ,int B)
+{
+  int power1;
+    power1=pow(A, B);
+    return power1;
+}
