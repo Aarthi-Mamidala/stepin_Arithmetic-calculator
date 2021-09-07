da8536e26d1bd3a625a9c1aaa3e4250fe837e2ed
Aarthi-Mamidala <89648059+Aarthi-Mamidala@users.noreply.github.com>
2021-09-07 19:12:42 +0530

    Update Division.c

--- 3_Implementation/src/Division.c
+++ 3_Implementation/src/Division.c
@@ -1,1 +1,20 @@
+#include "Function.h"
+/**
+ * @brief  Division of two numbers
+ * 
+ * @param A operand1
+ * @param B operand2
+ * @return int  it returns the output value
+ */
 
+int Div(int A ,int B)
+{
+     if(B==0)
+    {
+        return 0;
+    }
+    else
+    {
+    return A/B;
+    }
+}
