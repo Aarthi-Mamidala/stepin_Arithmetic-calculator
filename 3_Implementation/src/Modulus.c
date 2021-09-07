762916fc68b529999d762677a630a2e28a2ebb66
Aarthi-Mamidala <89648059+Aarthi-Mamidala@users.noreply.github.com>
2021-09-07 19:10:53 +0530

    Update Modulus.c

--- 3_Implementation/src/Modulus.c
+++ 3_Implementation/src/Modulus.c
@@ -1,1 +1,15 @@
-
+#include"Function.h"
+/**
+ * @brief  function to find modulus of two numbers
+ * 
+ * @param A operand1
+ * @param B operand2
+ * @return int  it returns the output value
+ */
+int Mod(int A ,int B)
+{
+        int REM=0;
+        REM=A%B;
+        return REM;
+    
+}
