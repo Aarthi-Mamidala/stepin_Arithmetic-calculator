5764091fd5ba20543d35123a1dbbd7f2af4f6713
Aarthi-Mamidala <89648059+Aarthi-Mamidala@users.noreply.github.com>
2021-09-07 19:08:25 +0530

    Update Subtraction.c

--- 3_Implementation/src/Subtraction.c
+++ 3_Implementation/src/Subtraction.c
@@ -1,1 +1,12 @@
-
+#include "Function.h"
+/**
+ * @brief  function to subtraction of two numbers
+ * 
+ * @param A operand1
+ * @param B operand2
+ * @return int  it returns the output value
+ */
+ int Sub(int A ,int B)
+{
+    return A-B;
+}
