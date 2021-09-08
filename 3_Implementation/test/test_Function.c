7fa0cc6cd91748e86934d3ddf345dea600c591a4
Aarthi-Mamidala <89648059+Aarthi-Mamidala@users.noreply.github.com>
2021-09-08 11:30:15 +0530

    Update test_Function.c

--- 3_Implementation/test/test_Function.c
+++ 3_Implementation/test/test_Function.c
@@ -9,52 +9,52 @@ void tearDown(){}
 
 void test_C(void)
 {
-  TEST_ASSERT_EQUAL(25, C(10,15));
-  TEST_ASSERT_EQUAL(775, C(25,750));
+  TEST_ASSERT_EQUAL(25, Addition(10,15));
+  TEST_ASSERT_EQUAL(775, Addition(25,750));
 }
 
 void test_D(void)
 {
-  TEST_ASSERT_EQUAL(14, D(20,6));
-  TEST_ASSERT_EQUAL(90, D(150,60));
+  TEST_ASSERT_EQUAL(14, Subtraction(20,6));
+  TEST_ASSERT_EQUAL(90, Subtraction(150,60));
 }
 
-void test_E(void)
+void test_Multiplication(void)
 {
-  TEST_ASSERT_EQUAL(60, E(10,6));
-  TEST_ASSERT_EQUAL(200, E(10,20));
+  TEST_ASSERT_EQUAL(60, Multiplication(10,6));
+  TEST_ASSERT_EQUAL(200, Multiplication(10,20));
 }
 
-void test_E_zero(void)
+void test_Multiplication_zero(void)
 {
-  TEST_ASSERT_EQUAL(0, Mul1(16,0));
-  TEST_ASSERT_EQUAL(0, Mul1(20,0));
+  TEST_ASSERT_EQUAL(0, Multiplication(16,0));
+  TEST_ASSERT_EQUAL(0,Multiplication(20,0));
 }
 
-void test_F(void)
+void test_Division(void)
 {
-  TEST_ASSERT_EQUAL(5, F(50,10));
-  TEST_ASSERT_EQUAL(17, F(85,5));
+  TEST_ASSERT_EQUAL(5,Division(50,10));
+  TEST_ASSERT_EQUAL(17, Division(85,5));
 }
 
-void test_F_zero(void)
+void test_Division_zero(void)
 {
-  TEST_ASSERT_EQUAL(0, F(57,0));
-  TEST_ASSERT_EQUAL(0,F(39,0));
-}
-
-
-void test_H(void)
-{
-  TEST_ASSERT_EQUAL(25, H(5,2));
-  TEST_ASSERT_EQUAL(225, H(15,2));
+  TEST_ASSERT_EQUAL(0, Division(57,0));
+  TEST_ASSERT_EQUAL(0,Division(39,0));
 }
 
 
-void test_G(void)
+void test_Power(void)
 {
-  TEST_ASSERT_EQUAL(1, G(26,5));
-  TEST_ASSERT_EQUAL(2, G(127,5));
+  TEST_ASSERT_EQUAL(25, Power(5,2));
+  TEST_ASSERT_EQUAL(225, Power(15,2));
+}
+
+
+void test_Modulus(void)
+{
+  TEST_ASSERT_EQUAL(1, Modulus(26,5));
+  TEST_ASSERT_EQUAL(2, Modulus(127,5));
 }
 
 
@@ -64,14 +64,14 @@ int main(void)
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_C);
-  RUN_TEST(test_D);
-  RUN_TEST(test_E);
-  RUN_TEST(test_E_zero);
-  RUN_TEST(test_F);
-  RUN_TEST(test_F_zero);
-  RUN_TEST(test_H);
-  RUN_TEST(test_G);
+  RUN_TEST(test_Addition);
+  RUN_TEST(test_Subtraction);
+  RUN_TEST(test_Multiplication);
+  RUN_TEST(test_Multiplication_zero);
+  RUN_TEST(test_Division);
+  RUN_TEST(test_Division_zero);
+  RUN_TEST(test_Power);
+  RUN_TEST(test_Modulus);
   
   
   /* Close the Unity Test Framework */
